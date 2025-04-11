71f5a998cd252a0a36194fe0f8ca7eff76b28dee
Quan Ming Vang <mingvang2003@gmail.com>
2025-04-11 16:32:05 +0100

    Player class added.

--- main.cpp
+++ main.cpp
@@ -2,13 +2,6 @@
 #include <string_view>
 
 class Creature {
-private:
-  std::string m_name{};
-  char m_symbol{};
-  int m_health{};
-  int m_dmg{};
-  int m_gold{};
-
 public:
   Creature(std::string_view name, char symbol, int health, int dmg, int gold)
       : m_name{name}, m_symbol{symbol}, m_health{health}, m_dmg{dmg},
@@ -25,15 +18,44 @@ public:
   bool isDead() { return m_health <= 0; }
 
   void addGold(int gold) { m_gold += gold; }
+
+protected:
+  std::string m_name{};
+  char m_symbol{};
+  int m_health{};
+  int m_dmg{};
+  int m_gold{};
+};
+
+class Player : public Creature {
+public:
+  Player(std::string_view name, char symbol = '@', int health = 10, int dmg = 1,
+         int gold = 0)
+      : Creature{name, symbol, health, dmg, gold} {}
+
+  void levelUp() {
+    m_level++;
+    m_dmg++;
+  }
+
+  int level() { return m_level; }
+
+  bool hasWon() { return m_level == 20; }
+
+private:
+  int m_level{1};
 };
 
 int main() {
-  Creature o{"orc", 'o', 4, 2, 10};
+  std::cout << "Enter your name: ";
+  std::string name;
+  std::cin >> name;
 
-  o.addGold(5);
-  o.reduceHealth(1);
-  std::cout << "The " << o.name() << " has " << o.health()
-            << " health and is carrying " << o.gold() << " gold.\n";
+  Player Alex{name};
+
+  std::cout << "Welcome, " << name << ".\n";
+  std::cout << "You have " << Alex.health() << " health and are carrying "
+            << Alex.gold() << " gold.\n";
 
   return 0;
 }
