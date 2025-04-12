1becf65d72c0e5704fe0f574edb3aba60f1a5e48
Quan Ming Vang <mingvang2003@gmail.com>
2025-04-12 14:43:01 +0100

    Game logic added, game is playable.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "random.h"
-#include <algorithm>
 #include <iostream>
 #include <string_view>
 
@@ -74,11 +73,82 @@ private:
   static_assert(max_types == std::ssize(monsterData));
 };
 
+std::string getUserInput() {
+  std::cout << "Enter your name: ";
+  std::string name;
+  std::cin >> name;
+  return name;
+}
+
+void attackPlayer(Monster &m, Player &p) {
+
+  m.reduceHealth(p.dmg());
+  std::cout << "You hit the " << m.name() << " for " << p.dmg() << " damage.\n";
+
+  if (m.isDead()) {
+
+    std::cout << "You killed the " << m.name() << ".\n";
+
+    p.levelUp();
+    std::cout << "You are now level " << p.level() << ".\n";
+
+    p.addGold(m.gold());
+    std::cout << "You found " << m.gold() << " gold.\n";
+  }
+}
+void attackMonster(Monster &m, Player &p) {
+
+  p.reduceHealth(m.dmg());
+  std::cout << "The " << m.name() << " has hit you for " << m.dmg() << ".\n";
+}
+
+void fightMonster(Player &p) {
+
+  Monster m{Monster::getRandomMonster()};
+
+  std::cout << "You have encountered a " << m.name() << " (" << m.symbol()
+            << ").\n";
+
+  while (true) {
+    if (m.isDead() || p.isDead() || p.hasWon())
+      break;
+
+    std::cout << "(R)un or (F)ight: ";
+    char input{};
+    std::cin >> input;
+
+    if (input == 'f') {
+      attackPlayer(m, p);
+      attackMonster(m, p);
+    }
+
+    if (input == 'r') {
+      bool success = static_cast<bool>(Random::get(0, 1));
+      if (success) {
+        std::cout << "You successfully fled.\n";
+        break;
+      } else {
+        std::cout << "You failed to flee.\n";
+        attackMonster(m, p);
+      }
+    }
+  }
+}
+
 int main() {
 
-  for (int i{0}; i < 10; ++i) {
-    Monster m{Monster::getRandomMonster()};
-    std::cout << "A " << m.name() << " (" << m.symbol() << ") was created.\n";
+  // Init player
+  Player player{getUserInput()};
+  std::cout << "Welcome, " << player.name() << '\n';
+
+  while (true) {
+    if (player.isDead()) {
+      std::cout << "You died at level " << player.level() << " and with "
+                << player.gold() << " gold.\n";
+      break;
+    }
+
+    fightMonster(player);
   }
 
   return 0;
